f2073368d28c7984dbdda21af0003610b4435566
hendisantika <hendisantika@yahoo.co.id>
2019-06-20 06:37:04 +0700

    #3 - Add QuickUnion case

---
+++ src/main/java/com/hendisantika/demo/App.java
@@ -0,0 +1,13 @@
+package com.hendisantika.demo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : data-structure-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-20
+ * Time: 06:34
+ */
+public class App {
+}
